55528e1ad48e8b30ae6768365f1a4514bde094cb
MarkNewcomb1 <wlmmn@hotmail.com>
2017-11-28 17:13:35 -0700

    portfolio website update

--- index.php
+++ index.php
@@ -35,6 +35,9 @@
 				$('#hotrod').click(function() {
 			window.open('http://hotroddiy.com', '_blank');
 		});
+				$('#flora-finder').click(function() {
+			window.open('https://github.com/MarkNewcomb1/flora', '_blank');
+		});		
 	  });
   </script>
 
@@ -52,13 +55,13 @@
     <div class="container">
       <div class="row">
         <div class="one-half column">
-          <h4 class="hero-heading">Hi. I'm Mark Newcomb, Full Stack Web Developer.</h4>
-<!--           <a class="button button-primary" href="http://getskeleton.com">Try Skeleton</a> -->
+          <h4 class="hero-heading">Hi. I'm Mark Newcomb, Front-End Web Developer.</h4>
+          <h4 class="hero-heading">I've worked on household brand websites.</h4>          
 <a href="https://primepathlab.com" target="_blank"><img class="u-max-full-width" src="images/primepath-desktop-low.jpg"></a>
         </div>
         <div class="one-half column phones">
           <img class="phone" id="hotrod" src="images/iphone-hotrod-low.png">
-          <img class="phone" src="images/iphone-flora-low.png">
+<!--           <img class="phone" src="images/iphone-flora-low.png"> -->
         </div>
       </div>
     </div>
@@ -68,29 +71,30 @@
     <div class="container">
       <div class="row">
         <div class="one-third column value">
-          <h2 class="value-multiplier">2</h2>
+          <h2 class="value-multiplier">3+</h2>
           <h5 class="value-heading">Years' Experience</h5>
           <p class="value-description">Employed with Web Development companies in Colorado</p>
         </div>
         <div class="one-third column value">
           <h2 class="value-multiplier">3</h2>
           <h5 class="value-heading">Formal Certificates</h5>
-          <p class="value-description">Professional certificate in Full Stack Web Development (MongoDB, Express, Angular, Node, React), Frontend Bootcamp (HTML5, CSS, Javascript) and Backend Bootcamp (PHP, server-side architectures)</p>
+          <p class="value-description">Professional certificate in Full Stack Web Development (MongoDB, Express, Angular, Node, React), Frontend Bootcamp (HTML5, SASS/CSS, Javascript) and Backend Bootcamp (PHP, server-side architectures)</p>
         </div>
         <div class="one-third column value">
-          <h2 class="value-multiplier">2</h2>
-          <h5 class="value-heading">Cool for Non-Responsive</h5>
-          <p class="value-description">Mobile-first web design is my approach, with Photoshop/Illustrator skills and a lifelong musician's artistic sensibilities</p>
+          <h2 class="value-multiplier">6</h2>
+          <h5 class="value-heading">Global brands</h5>
+          <p class="value-description">That I've been entrusted to do website builds for, whose names you've seen on store shelves. Some have been full builds; others have been integration of new features into existing codebases. <br>Either way, I get it done.</p>
         </div>
-      </div>
+      </div> <!-- row -->
     </div>
   </div>
-  <div class="section get-help">
+  <div class="section get-help flora">
     <div class="container">
       <h3 class="section-heading">View my GitHub or Linkedin</h3>
       <p class="section-description">I've created an PHP-based MVC for collecting flora information in Colorado's wilderness, complete with HTML5 and PHP input validation, Open Weather API, and geolocation. Very little framework; mostly from-scratch code.</p>
       <a class="button button-primary" href="https://github.com/MarkNewcomb1/flora" target="_blank">View the Source</a>
       <a class="button button-primary" href="https://www.linkedin.com/in/marknewcomb1" target="_blank">View My LinkedIn</a>
+      <img class="u-max-full-width" id="flora-finder" src="images/flora-finder-desktop.png">
     </div>
   </div>
     <div class="section get-help react">
@@ -98,25 +102,26 @@
       <h3 class="section-heading">React.js Music Collection/Search App</h3>
       <p class="section-description">Using the Discogs API, I've created an app that allows you to keep track of your physical media collection. Built with React, Node, Express, jQuery and hand-built CSS; NO Bootstrap!</p>
       <a class="button button-primary" href="https://github.com/MarkNewcomb1/final-project" target="_blank">View the Source</a>
+      <a class="button button-primary" href="https://www.youtube.com/watch?v=gjayvebQerI" target="_blank">View Demo Video</a>
     </div>
   </div>
   <div class="section categories">
     <div class="container">
-      <h3 class="section-heading">Of Course, I Know Desktop Too</h3>
+      <h3 class="section-heading">Of Course, I Know WordPress Too</h3>
       <p class="section-description">I have extensive experience maintaining legacy websites for clients, as well as putting Wordpress sites through server migrations, updates, PHP wrappers, etc.</p>
       <div class="row">
         <div class="one-half column category">
           <a href="http://kspath.com" target="_blank"><img class="u-max-full-width" src="images/kps-desktop-low.jpg"></a>
         </div>
         <div class="one-half column category">
-          <a href="#"><img class="u-max-full-width" src="images/marknewcomb1-desktop-low.jpg"></a>
+          <a href="http://affordalab.com" target="_blank"><img class="u-max-full-width" src="images/affordalab-desktop-low.jpg"></a>
         </div>
       </div>
     </div>
   </div>
-  
   <!-------------FORM--------->
   <div class="container">
+	        <h3 class="section-heading" style="text-align:center;padding-top:10rem;">Want to see more examples? Send me an email!</h3>
 <form name="contactform" method="post" action="send_form_email.php">
   <div class="row">
     <div class="six columns">
@@ -133,12 +138,6 @@
   </div>
   <label for="exampleMessage">Message</label>
   <textarea class="u-full-width" required placeholder="Hi Mark …" id="exampleMessage" name="exampleMessage"></textarea>
-<!--
-  <label class="example-send-yourself-copy">
-    <input type="checkbox">
-    <span class="label-body">Send a copy to yourself</span>
-  </label>
--->
   <input class="button-primary" type="submit" value="Submit">
 </form>
   </div><!--end div container for the form----->
@@ -146,7 +145,7 @@
   
   
   
-<footer><p>Footer stuff!</p></footer>
+<footer><p></p></footer>
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
