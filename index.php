1b449a253ca29afc03f23d01eed4116147b14a72
MarkNewcomb1 <wlmmn@hotmail.com>
2022-04-26 18:57:44 -0600

    much needed copy update

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
       <div class="row">
         <div class="one-half column">
           <h4 class="hero-heading">Hi. I'm Mark Newcomb, <br>UI Developer.</h4>
-          <h4 class="hero-heading">I've worked on household brand websites and legal software.</h4>    
+          <h4 class="hero-heading">I've worked on household brand websites, administrator tools for DaaS (Directory-as-a-Service), and legal software.</h4>    
           <a href="http://kspath.com" target="_blank"><img class="u-max-full-width" src="images/kps-desktop-low.jpg"></a>              
         </div>
         <div class="one-half column phones">
@@ -71,7 +71,7 @@
     <div class="container">
       <div class="row">
         <div class="one-third column value">
-          <h2 class="value-multiplier">5+</h2>
+          <h2 class="value-multiplier">8+</h2>
           <h5 class="value-heading">Years' Experience</h5>
           <p class="value-description">with Software companies in Colorado - REAL work for REAL clients.</p>
         </div>
@@ -97,7 +97,7 @@
       <img class="u-max-full-width" id="flora-finder" src="images/flora-finder-desktop.jpg">
     </div>
   </div>
-    <div class="section get-help react">
+    <div class="section get-help categories">
     <div class="container">
       <h3 class="section-heading">React.js Music Collection/Search App</h3>
       <p class="section-description">I've made an app for keeping track of your physical media collection. Built with Discogs API, React, Node, Express, jQuery and hand-built CSS - NO Bootstrap!</p>
@@ -105,47 +105,6 @@
       <a class="button button-primary" href="https://www.youtube.com/watch?v=gsKL-Dx6mRc" target="_blank">Demo Video</a>
     </div>
   </div>
-  <div class="section categories">
-    <div class="container">
-      <h3 class="section-heading">Of Course, I Know WordPress Too</h3>
-      <p class="section-description">I have extensive experience maintaining legacy websites for clients, as well as putting Wordpress sites through server migrations, updates, etc.</p>
-      <div class="row">
-        <div class="one-half column category">
-          <a href="https://primepathlab.com" target="_blank"><img class="u-max-full-width" src="images/primepath-desktop-low.jpg"></a>
-        </div>
-        <div class="one-half column category">
-          <a href="http://affordalab.com" target="_blank"><img class="u-max-full-width" src="images/affordalab-desktop-low.jpg"></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-------------FORM--------->
-  <div class="container">
-	        <h3 class="section-heading" style="text-align:center;padding-top:10rem;">Want to see more? Send me an email!</h3>
-<form name="contactform" method="post" action="send_form_email.php">
-  <div class="row">
-    <div class="six columns">
-      <label for="exampleEmailInput">Your email</label>
-      <input class="u-full-width" required type="email" placeholder="name@example.com" id="exampleEmailInput" name="exampleEmailInput">
-    </div>
-    <div class="six columns">
-      <label for="exampleRecipientInput">Reason for contacting</label>
-      <select class="u-full-width" id="exampleRecipientInput" name="exampleRecipientInput">
-        <option value="Option 1">Question</option>
-        <option value="Option 2">Job Offer</option>
-      </select>
-    </div>
-  </div>
-  <label for="exampleMessage">Message</label>
-  <textarea class="u-full-width" required placeholder="Hi Mark …" id="exampleMessage" name="exampleMessage"></textarea>
-  <input class="button-primary" type="submit" value="Submit">
-</form>
-  </div><!--end div container for the form----->
-  <!--------END FORM---------->
-  
-  
-  
-<footer><p></p></footer>
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
