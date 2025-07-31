4b9093880e2688cf60f21ebc2e3c5f4efb988051
MarkNewcomb1 <wlmmn@hotmail.com>
2025-07-31 12:22:54 -0600

    scrolltrigger working

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
   gsap.from("#hero-text", {
-    duration: 1,
+    duration: 5,
     opacity: 0,
     y: 50,
     ease: "power3.out"
   });
+});
+
+const video = document.querySelector(".video-background");
+let src = video.currentSrc || video.src;
+console.log(video, src);
+
+/* Make sure the video is 'activated' on iOS */
+function once(el, event, fn, opts) {
+  var onceFn = function (e) {
+    el.removeEventListener(event, onceFn);
+    fn.apply(this, arguments);
+  };
+  el.addEventListener(event, onceFn, opts);
+  return onceFn;
+}
+
+once(document.documentElement, "touchstart", function (e) {
+  video.play();
+  video.pause();
+});
+
+/* ---------------------------------- */
+/* Scroll Control! */
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+  defaults: { duration: 1 },
+  scrollTrigger: {
+    trigger: "#container",
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true
+  }
+});
+
+once(video, "loadedmetadata", () => {
+  tl.fromTo(
+    video,
+    {
+      currentTime: 0
+    },
+    {
+      currentTime: video.duration || 1
+    }
+  );
 });
