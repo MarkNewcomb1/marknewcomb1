cd5c4cd271e0db0c6a0d3123212dda0ad3dc2219
MarkNewcomb1 <wlmmn@hotmail.com>
2025-07-31 19:13:41 -0600

    re-rendered videos and added scrolltrigger logic

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  gsap.from("#hero-text", {
+  gsap.from(".hero-heading", {
     duration: 5,
     opacity: 0,
     y: 50,
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// ffmpeg -i goozmo2014-1.mp4 -movflags faststart -vcodec libx264 -crf 23 -g 1 -pix_fmt yuv420p output.mp4
 const video = document.querySelector(".video-background");
 let src = video.currentSrc || video.src;
 console.log(video, src);
@@ -34,16 +35,72 @@ gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline({
   defaults: { duration: 1 },
   scrollTrigger: {
-    trigger: "#container",
+    trigger: "#container1",
     start: "top top",
-    end: "2800",
-    scrub: true
+    end: "3000",
+    scrub: true,
   }
 });
 
 once(video, "loadedmetadata", () => {
   tl.fromTo(
     video,
+    { currentTime: 0 },
+    { currentTime: video.duration || 1, ease: "none" })
+    .to(".hero-content-wrapper", {
+    autoAlpha: 0,           // fade + hide from accessibility/pointer events
+    duration: 0.12,
+    ease: "none",
+    immediateRender: false
+  }, "-=0.12");
+});
+
+
+
+
+
+
+
+
+
+
+const video2 = document.querySelector(".video-background-2");
+let src2 = video2.currentSrc || video2.src;
+console.log(video2, src);
+
+/* Make sure the video is 'activated' on iOS */
+function once(el, event, fn, opts) {
+  var onceFn = function (e) {
+    el.removeEventListener(event, onceFn);
+    fn.apply(this, arguments);
+  };
+  el.addEventListener(event, onceFn, opts);
+  return onceFn;
+}
+
+once(document.documentElement, "touchstart", function (e) {
+  video2.play();
+  video2.pause();
+});
+
+/* ---------------------------------- */
+/* Scroll Control! */
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl2 = gsap.timeline({
+  defaults: { duration: 1 },
+  scrollTrigger: {
+    trigger: "#container2",
+    start: "top top",
+    end: "8000",
+    scrub: true
+  }
+});
+
+once(video2, "loadedmetadata", () => {
+  tl2.fromTo(
+    video2,
     {
       currentTime: 0
     },
